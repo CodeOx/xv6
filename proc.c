ff19ad2d8a14f6244c0a4c3cf35deabc560442e7
Chetan Mittal <mittalchetan98@gmail.com>
2019-02-16 18:03:45 +0530

    added sleeping processes to ps

--- proc.c
+++ proc.c
@@ -542,7 +542,7 @@ ps(void)
   acquire(&ptable.lock);
 
   for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
-    if(p->state == RUNNABLE || p->state == RUNNING)
+    if(p->state == RUNNABLE || p->state == RUNNING || p->state == SLEEPING)
       cprintf("pid:%d name:%s\n", p->pid, p->name);
 
   release(&ptable.lock);
