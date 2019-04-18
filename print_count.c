06a507364b99c3e6e408a1fcd4bf02ebb00a0c59
Chetan Mittal <mittalchetan98@gmail.com>
2019-04-18 14:49:32 +0530

    updated print count for ls

--- print_count.c
+++ print_count.c
@@ -7,7 +7,15 @@ main(int argc ,char* argv[])
 {
 	ps();
 	int id=create_container();
-	join_container(id);
-	ps();
+	int child = fork();
+	if(child == 0){
+		join_container(id);
+		ps();
+		char *argv[] = {"ls", 0};
+		exec("/ls",argv);
+		printf(1, "child : ls exec failed\n");
+		exit();
+	}
+	wait();
 	exit () ;
 }
